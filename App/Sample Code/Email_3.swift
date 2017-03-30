24aaa35a856360d19a9d2ed3b33e16d1c6a1bfb2
karlsolomon <ksolomon@utexas.edu>
2017-03-30 07:34:49 -0500

    email file updated

--- App/Sample Code/Email_3.swift
+++ App/Sample Code/Email_3.swift
@@ -32,3 +32,50 @@ class ArchiveFile {
     }
 
 }
+
+
+
+import Foundation
+import UIKit
+import MessageUI
+ 
+class EmailViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    var recordingDetails = ""
+    var recordingDetailsArr = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        recordingDetailsArr = recordingDetails.componentsSeparatedByString(",")
+    }
+    
+    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["karl.solomon20@gmail.com"])
+        mailComposerVC.setSubject("EKG Recording")
+        mailComposerVC.setMessageBody("Time: \(recordingDetailsArr[0]) \nSymptoms: \(recordingDetailsArr[1])", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
