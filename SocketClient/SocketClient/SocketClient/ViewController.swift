3a6cdc929df5d34279359b8397efb3aaaa19cb6f
petermyu <pmy89@lib-pclem026.lib.utexas.edu>
2017-04-06 18:11:09 -0500

    csv file read and parsed into lists

--- SocketClient/SocketClient/SocketClient/ViewController.swift
+++ SocketClient/SocketClient/SocketClient/ViewController.swift
@@ -32,43 +32,71 @@ class ViewController: UIViewController, NSStreamDelegate{
         inputStream.open()
         outputStream.open()
         var buffer : [UInt8] = [97]
-        var readBytes = [UInt8](count: 5, repeatedValue: 0)
+        var readBytes = [UInt8](count: 60000, repeatedValue: 0)
         var dataList = [String]()
         var flag = true
         while flag{
         while inputStream.hasBytesAvailable{
-            inputStream.read(&readBytes, maxLength: 5)
+            inputStream.read(&readBytes, maxLength: 60000)
             outputStream.write(buffer, maxLength: buffer.count)
-            var ascii = convertFromAscii(readBytes)
+            let ascii = convertFromAscii(readBytes)
             print(ascii)
-            if ascii == "end"{
-                flag = false
-                break;
-            }
-            else{
-                
-            dataList.append(ascii)
-            readBytes = [UInt8](count: 5, repeatedValue: 0)
-            }
+            dataList = ascii
+            flag = false
+            break
         }
-            
         }
         print(dataList)
+        let adjustedList = adjustList(dataList)
+        print(adjustedList)
     }
-    func convertFromAscii(buffer: [UInt8]) -> String{
+    func convertFromAscii(buffer: [UInt8]) -> [String]{
+        var list = [String]()
         var s = String()
         for value in buffer{
             if value != 0{
-                if value == 33{
-                    return "end"
+                if value == 44 || value == 10{//delimiter is a comma
+                    list.append(s)
+                    s = ""
                 }
                 else{
                     s += String(UnicodeScalar(value))
                 }
             }
         }
-        return s
+        return list
     }
+    func adjustList(list : [String]) -> [String]{
+        var temp = list
+        var size = list.count
+        var list1 = [String]()
+        var list2 = [String]()
+        var list3 = [String]()
+        var list4 = [String]()
+        var x = 1
+        for i in 0..<size{
+            if x == 1 {
+                list1.append(temp.removeAtIndex(0))
+                x+=1
+            }
+            else if x == 2 {
+                list2.append(temp.removeAtIndex(0))
+                x+=1
+            }
+            else if x == 3 {
+                list3.append(temp.removeAtIndex(0))
+                x+=1
+            }
+            else if x == 4{
+                list4.append(temp.removeAtIndex(0))
+                x=1
+            }
+            
+        }
+        
+        return list1+list2+list3+list4
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
