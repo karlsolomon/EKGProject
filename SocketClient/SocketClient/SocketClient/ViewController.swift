72b5f76c2fd2d6369520f59669ae05b3faf13076
petermyu <pmy89@lib-pclem026.lib.utexas.edu>
2017-04-06 18:58:14 -0500

    CSV file created and stored locally!

--- SocketClient/SocketClient/SocketClient/ViewController.swift
+++ SocketClient/SocketClient/SocketClient/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController, NSStreamDelegate{
         outputStream.open()
         var buffer : [UInt8] = [97]
         var readBytes = [UInt8](count: 60000, repeatedValue: 0)
-        var dataList = [String]()
+        var dataList = String()
         var flag = true
         while flag{
         while inputStream.hasBytesAvailable{
@@ -47,56 +47,38 @@ class ViewController: UIViewController, NSStreamDelegate{
         }
         }
         print(dataList)
-        let adjustedList = adjustList(dataList)
-        print(adjustedList)
+        writeCSV(dataList)
+    //    let adjustedList = adjustList(dataList)
+    //    print(adjustedList)
     }
-    func convertFromAscii(buffer: [UInt8]) -> [String]{
+    func convertFromAscii(buffer: [UInt8]) -> String{
         var list = [String]()
         var s = String()
         for value in buffer{
             if value != 0{
-                if value == 44 || value == 10{//delimiter is a comma
-                    list.append(s)
-                    s = ""
-                }
-                else{
-                    s += String(UnicodeScalar(value))
-                }
+                s += String(UnicodeScalar(value))
             }
         }
-        return list
+        return s
     }
-    func adjustList(list : [String]) -> [String]{
-        var temp = list
-        var size = list.count
-        var list1 = [String]()
-        var list2 = [String]()
-        var list3 = [String]()
-        var list4 = [String]()
-        var x = 1
-        for i in 0..<size{
-            if x == 1 {
-                list1.append(temp.removeAtIndex(0))
-                x+=1
-            }
-            else if x == 2 {
-                list2.append(temp.removeAtIndex(0))
-                x+=1
-            }
-            else if x == 3 {
-                list3.append(temp.removeAtIndex(0))
-                x+=1
-            }
-            else if x == 4{
-                list4.append(temp.removeAtIndex(0))
-                x=1
-            }
-            
+
+    func writeCSV(data: String) -> Bool{
+        var fileName = "sample.csv"
+        if let filePath = NSBundle.mainBundle().pathForResource("sample", ofType: "csv"){
+            fileName = filePath
         }
-        
-        return list1+list2+list3+list4
+        else {
+            fileName = NSBundle.mainBundle().bundlePath + fileName
+        }
+        print(fileName)
+        //write the file, return true if it works, false otherwise.
+        do{
+            try data.writeToFile(fileName, atomically: true, encoding: NSUTF8StringEncoding )
+            return true
+        } catch{
+            return false
+        }
     }
-        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
