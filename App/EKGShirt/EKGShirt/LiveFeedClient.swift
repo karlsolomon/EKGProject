55c69eb1576601192775e272c4db66641c6ae3f8
petermyu <bme261l@karls-mac-mini.local>
2017-04-27 02:56:07 -0500

    updated opening LiveFeedViewController. need to work on socket stuff for live feed

--- App/EKGShirt/EKGShirt/LiveFeedClient.swift
+++ App/EKGShirt/EKGShirt/LiveFeedClient.swift
@@ -16,37 +16,3 @@
 
 import Foundation
 
-class LiveFeedClient {
-    
-    final let encoding = NSUTF8StringEncoding
-    var leads = [String:[Int]]()
-    var text = String()
-    var header = [String]()
-    var initial = [Int]()
-    
-    //Parses CSV File in memory as Map of [LeadName : [LeadData]]
-    init(url: NSURL) {
-        do {
-            self.text = try String(contentsOfURL: url, encoding: encoding)
-            let rows = text.componentsSeparatedByString("\r\n")
-            for k in 0..<rows.count {
-                var values = rows[k].componentsSeparatedByString(",")
-                let leadName = values.removeAtIndex(0)
-                self.leads[leadName] = values.map{Int($0)!}
-            }
-        } catch {
-            print("Could Not Find File")
-        }
-    }
-    
-    //Returns EKG map keyed on Lead Name, valued on lead data
-    func getLeads() -> [String: [Int]]{
-        return self.leads
-    }
-    
-    //Returns Lead Names
-    func getHeader() -> [String] {
-        return Array(self.leads.keys)
-    }
-    
-}
--- App/EKGShirt/EKGShirt/TabBarController.swift
+++ App/EKGShirt/EKGShirt/TabBarController.swift
@@ -20,4 +20,65 @@ class TabBarController : UITabBarController {
             print("Live Feed Selected")
         }
     }
+    
+    class LiveFeedClient {
+        let addr = "172.16.25.116"
+        let port = 8080
+        var inp : NSInputStream?
+        var out :NSOutputStream?
+        var fileName = String()
+        var filePath = NSURL()
+        var liveFeed = LiveFeedViewController()
+        
+        init(storyboard :UIStoryboard) {
+            liveFeed = storyboard.instantiateViewControllerWithIdentifier("LiveFeedViewController") as! LiveFeedViewController
+
+            
+            
+            NSStream.getStreamsToHostWithName(addr, port: port, inputStream: &inp, outputStream: &out)
+            
+            let inputStream = inp!
+            let outputStream = out!
+            
+            inputStream.open()
+            outputStream.open()
+            var buffer : [UInt8] = [97]
+            var readBytes = [UInt8](count: 2000, repeatedValue: 0)
+            var flag = true
+            var dataString = String()
+            var ascii = String()
+//FIXME
+            while flag{
+                outputStream.write(buffer, maxLength: buffer.count)
+                while inputStream.hasBytesAvailable{
+                    print(inputStream.read(&readBytes, maxLength: 2000))
+                    outputStream.write(buffer, maxLength: buffer.count)
+                    ascii = convertFromAscii(readBytes)
+                    dataString.appendContentsOf(ascii)
+                    
+                    if !inputStream.hasBytesAvailable{
+                        flag = false
+                        break
+                    }
+                }
+            
+            print(dataString)
+            let values = dataString.componentsSeparatedByString(",")
+            LiveFeedViewController.liveFeedData = values.map{ Int($0)!}
+            liveFeed.updateChartWithData()
+        
+            }
+        }
+        private func convertFromAscii(buffer: [UInt8]) -> String{
+            var list = [String]()
+            var s = String()
+            for value in buffer{
+                if value != 0{
+                    s += String(UnicodeScalar(value))
+                }
+            }
+            return s
+        }
+        
+    }
 }
